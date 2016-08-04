6cdf0dc27e6bb4d781f4038fff60bd1431154970
Torsten Werner <torsten.werner@westernacher.com>
2016-08-04 13:56:47 +0200

    simplify project setup

--- src/test/java/xyz/its_me/alfresco/NodeServiceTest.java
+++ src/test/java/xyz/its_me/alfresco/NodeServiceTest.java
@@ -1,24 +1,17 @@
 package xyz.its_me.alfresco;
 
-import org.alfresco.repo.web.scripts.TestWebScriptRepoServer;
 import org.alfresco.service.cmr.repository.NodeService;
-import org.junit.Before;
 import org.junit.Test;
 import org.springframework.context.ApplicationContext;
-import org.springframework.extensions.webscripts.TestWebScriptServer;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.junit.Assert.assertThat;
 
 public class NodeServiceTest {
-    private NodeService nodeService;
-
-    @Before
-    public void setUp() throws Exception {
-        final TestWebScriptServer server = TestWebScriptRepoServer.getTestServer();
-        final ApplicationContext context = server.getApplicationContext();
-        nodeService = context.getBean("nodeService", NodeService.class);
-    }
+    private static final String CONFIG_LOCATION = "classpath:alfresco/application-context.xml";
+    private static final ApplicationContext context = new ClassPathXmlApplicationContext(CONFIG_LOCATION);
+    private NodeService nodeService = context.getBean("nodeService", NodeService.class);
 
     @Test
     public void testContext() {
