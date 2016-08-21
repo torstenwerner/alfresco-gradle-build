dafc85984460ab4e6638f76c542ce6616570ee03
Torsten Werner <torsten.werner@westernacher.com>
2016-08-21 10:52:19 +0200

    add some actual main code and a test for it

--- src/test/java/xyz/its_me/alfresco/NodeServiceTest.java
+++ src/test/java/xyz/its_me/alfresco/NodeServiceTest.java
@@ -4,11 +4,8 @@ import org.alfresco.service.cmr.repository.NodeRef;
 import org.alfresco.service.cmr.repository.NodeService;
 import org.alfresco.service.cmr.repository.StoreRef;
 import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import java.util.UUID;
 
@@ -16,9 +13,7 @@ import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.junit.Assert.*;
 
-@RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration("classpath:alfresco/application-context.xml")
-public class NodeServiceTest {
+public class NodeServiceTest extends AbstractSpringTest {
     @Autowired
     @Qualifier("nodeService")
     private NodeService nodeService;
---
+++ src/test/java/xyz/its_me/alfresco/SampleServiceTest.java
@@ -0,0 +1,17 @@
+package xyz.its_me.alfresco;
+
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.junit.Assert.assertThat;
+
+public class SampleServiceTest extends AbstractSpringTest {
+    @Autowired
+    private SampleService sampleService;
+
+    @Test
+    public void testRoot() throws Exception {
+        assertThat(sampleService.getRoot(), notNullValue());
+    }
+}
