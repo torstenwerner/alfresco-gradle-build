a8017b953179d725b37eb5a515c3130ab6080282
Torsten Werner <torsten.werner@westernacher.com>
2016-08-21 19:41:39 +0200

    implement a first http test

--- alfresco/src/test/java/xyz/its_me/alfresco/HttpTest.java
+++ alfresco/src/test/java/xyz/its_me/alfresco/HttpTest.java
@@ -3,14 +3,24 @@ package xyz.its_me.alfresco;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
+import org.springframework.web.client.RestTemplate;
 import xyz.its_me.TomcatLauncher;
 
+import java.util.Map;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.startsWith;
+import static org.junit.Assert.assertThat;
+
 public class HttpTest {
+    private static final String TOMCAT_PORT = "9274";
+    private static final String BASE_URL = "http://localhost:" + TOMCAT_PORT + "/alfresco";
+
     private static final TomcatLauncher launcher;
 
     static {
         // The test port should be a little bit random to avoid conflicts with other programs.
-        System.setProperty("tomcat.port", "9274");
+        System.setProperty("tomcat.port", TOMCAT_PORT);
         launcher = TomcatLauncher.create();
     }
 
@@ -28,6 +38,12 @@ public class HttpTest {
 
     @Test
     public void serverIsUp() throws Exception {
+        final RestTemplate restTemplate = new RestTemplate();
+        final String url = BASE_URL + "/s/api/server";
+        final Map response = restTemplate.getForObject(url, Map.class);
 
+        final Map data = (Map) response.get("data");
+        assertThat(data.get("edition"), is("Community"));
+        assertThat((String) data.get("version"), startsWith("5.2"));
     }
 }
