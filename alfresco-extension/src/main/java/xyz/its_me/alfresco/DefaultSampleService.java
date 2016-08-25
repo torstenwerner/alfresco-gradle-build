fe8aada9eab25acb2286875273c339c6e72fd2c7
Torsten Werner <torsten.werner@westernacher.com>
2016-08-25 07:57:38 +0200

    use the public NodeService bean

--- alfresco-extension/src/main/java/xyz/its_me/alfresco/DefaultSampleService.java
+++ alfresco-extension/src/main/java/xyz/its_me/alfresco/DefaultSampleService.java
@@ -7,18 +7,19 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
+import static org.alfresco.repo.security.authentication.AuthenticationUtil.runAsSystem;
+
 @Service
 public class DefaultSampleService implements SampleService {
     private final NodeService nodeService;
 
     @Autowired
-    public DefaultSampleService(@Qualifier("nodeService") NodeService nodeService) {
+    public DefaultSampleService(@Qualifier("NodeService") NodeService nodeService) {
         this.nodeService = nodeService;
-        System.out.printf("nodeService: %s\n", nodeService);
     }
 
     @Override
     public NodeRef getRoot() {
-        return nodeService.getRootNode(StoreRef.STORE_REF_WORKSPACE_SPACESSTORE);
+        return runAsSystem(() -> nodeService.getRootNode(StoreRef.STORE_REF_WORKSPACE_SPACESSTORE));
     }
 }
